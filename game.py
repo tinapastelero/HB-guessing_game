e690d0829cbec3c907f95a56a8cc2525ce098391
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 12:43:40 -0700

    Final program

--- game.py
+++ game.py
@@ -4,3 +4,13 @@ name = raw_input("What is your name? ")
 print "hello, %s" %(name)
 number = random.randint(1, 100)
 print number
+guess = None
+while guess != number:
+    guess = raw_input("Please guess a number from 1-100: ")
+    if int(guess) > number:
+        print "Your guess is too high."
+    elif int(guess) < number:
+        print "Your guess is too low."
+    else:
+        print "Congratulations!"
+        break
