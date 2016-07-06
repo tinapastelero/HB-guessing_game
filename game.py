d5da5d3736e64d122d23637f09b97e5836bed123
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 16:21:28 -0700

    Ask player to play again

--- game.py
+++ game.py
@@ -3,21 +3,30 @@ import random
 name = raw_input("What is your name? ")
 print "hello, %s" %(name)
 
-number = random.randint(1, 100)
+guess = None
+play = True
+
+number = random.randint(1,100)
 print number
 
-guess = None
 while guess != number:
-    try:
-        guess = int(raw_input("Please guess a number from 1-100: "))
-        if guess > 100 or guess < 1:
-            print "Your guess is not between 1-100."
-        elif guess > number:
-            print "Your guess is too high."
-        elif guess < number:
-            print "Your guess is too low."
-        else:
-            print "Congratulations!"
-            break
-    except ValueError:
-        print ("Oops that was not a valid number, try again.")
+    while play == True:  
+        try:
+            guess = int(raw_input("Please guess a number from 1-100: "))
+            if guess > 100 or guess < 1:
+                print "Your guess is not between 1-100."
+            elif guess > number:
+                print "Your guess is too high."
+            elif guess < number:
+                print "Your guess is too low."
+            else:
+                print "Congratulations!"
+                want_to_play = raw_input("Do you want to play again? YES/NO ")
+                if want_to_play == "NO":
+                    play = False
+                    break
+                number = random.randint(1,100)
+                print number                
+        except ValueError:
+            print ("Oops that was not a valid number, try again.")
+
