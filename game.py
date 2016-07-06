cec0aabfa0de1927301e744484c25f8bab25c9db
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 16:09:41 -0700

    Validation checks

--- game.py
+++ game.py
@@ -2,15 +2,22 @@ import random
 
 name = raw_input("What is your name? ")
 print "hello, %s" %(name)
+
 number = random.randint(1, 100)
 print number
+
 guess = None
 while guess != number:
-    guess = raw_input("Please guess a number from 1-100: ")
-    if int(guess) > number:
-        print "Your guess is too high."
-    elif int(guess) < number:
-        print "Your guess is too low."
-    else:
-        print "Congratulations!"
-        break
+    try:
+        guess = int(raw_input("Please guess a number from 1-100: "))
+        if guess > 100 or guess < 1:
+            print "Your guess is not between 1-100."
+        elif guess > number:
+            print "Your guess is too high."
+        elif guess < number:
+            print "Your guess is too low."
+        else:
+            print "Congratulations!"
+            break
+    except ValueError:
+        print ("Oops that was not a valid number, try again.")
